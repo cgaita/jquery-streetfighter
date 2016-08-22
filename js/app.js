f8dd875e65dce90555845c5882645b62cebccb89
cgaita <gaitacaroline@gmail.com>
2016-08-22 00:05:21 -0700

    commit three

--- js/app.js
+++ js/app.js
@@ -8,16 +8,39 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		console.log('mousedown');
-		//play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//show hadouken and animate it to the right of the screen
+		$('.ryu-cool').hide();
+		$('.hadouken').finish().show()
+		.animate(
+			{'left': '1020px'},
+			500,
+			function() {
+			$(this).hide();
+			$(this).css('left', '520px');
+		});
 	})
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		//ryu goes back to his ready position
+		$('.ryu-cool').hide();
+	});
+	$(document).keydown(function(event) {
+		if (event.keyCode == 88) {
+			$('.ryu-ready').hide();
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();
+		}
+	}).keyup(function(event) {
+		if (event.keyCode == 88) {
+			$('.ryu-still').show();
+			$('.ryu-cool').hide();
+		}
 	});
 });
+function playHadouken() {
+	$('#hadouken-sound')[0].volume=0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
